97ccacd65e828a187c754c8bd8e2ac3e1e5e73ee
caiolara090 <caio.jorge@hotmail.com.br>
2024-04-06 09:59:40 -0300

    Pagina_de_pagamento_acrescentada

---
+++ josesburguer/lib/Pagamento.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class PagamentoPage extends StatelessWidget {
+  final double total;
+
+  PagamentoPage({required this.total});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pagamento via PIX'),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Total do Pedido: R\$${total.toStringAsFixed(2)}',
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              child: QrImageView(
+                data: '!BIG CHEESE!',
+                version: QrVersions.auto,
+                size: 200.0,
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
