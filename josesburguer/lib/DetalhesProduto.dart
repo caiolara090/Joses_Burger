58fb19932a58aa8ab9c4013e3711c70ce61b7928
josedmass <josedmassucato@gmail.com>
2024-04-19 22:40:54 -0300

    Alinhando código de detalhes do Produto

--- josesburguer/lib/DetalhesProduto.dart
+++ josesburguer/lib/DetalhesProduto.dart
@@ -59,170 +59,124 @@ class _DetalhesPaginaState extends State<DetalhesPagina> {
       ),
 
       body: SingleChildScrollView(
-  child: Padding(
-    padding: EdgeInsets.all(16.0), // Adiciona espaço ao redor do conteúdo
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        if (widget.produto != null)
-          Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Image.asset(
-                  widget.produto.foto,
-                  fit: BoxFit.cover,
+        child: Padding(
+          padding: EdgeInsets.all(16.0), // Adiciona espaço ao redor do conteúdo
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              if (widget.produto != null)
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Image.asset(
+                        widget.produto.foto,
+                        fit: BoxFit.cover,
+                      ),
+                      SizedBox(height: 20),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        child: Text(
+                          widget.produto.nome ?? '',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ), 
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                SizedBox(height: 20),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
+              SizedBox(height: 16),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Text(
+                  widget.produto?.descricao ?? '', // Verifica se a descrição é nula
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ), 
+                ),
+              ),
+              SizedBox(height: 26),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Center(
                   child: Text(
-                    widget.produto.nome ?? '',
-                    textAlign: TextAlign.center,
+                    'R\$ ${widget.produto?.preco.toStringAsFixed(2) ?? ''}',
+                    textAlign: TextAlign.justify,
                     style: TextStyle(
-                      fontSize: 20,
+                      fontSize: 25,
                       fontWeight: FontWeight.bold,
                       color: Colors.black,
                     ), 
                   ),
                 ),
-              ],
-            ),
-          ),
-        SizedBox(height: 16),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          child: Text(
-            widget.produto?.descricao ?? '', // Verifica se a descrição é nula
-            textAlign: TextAlign.justify,
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ), 
-          ),
-        ),
-        SizedBox(height: 26),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          child: Center(
-            child: Text(
-              'R\$ ${widget.produto?.preco.toStringAsFixed(2) ?? ''}',
-              textAlign: TextAlign.justify,
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ), 
-            ),
-          ),
-        ),
-        SizedBox(height: 16),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          child: ElevatedButton(
-            onPressed: () {
-              if (widget.produto != null) { // Verifica se o produto não é nulo
-                bool pedidoExistente = false;
+              ),
+              SizedBox(height: 16),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (widget.produto != null) { // Verifica se o produto não é nulo
+                      bool pedidoExistente = false;
 
-                for (var pedido in widget.pedidos) {
-                  if (pedido.nome == widget.produto.nome) {
-                    pedido.adicionarPedido();
-                    pedidoExistente = true;
-                    break;
-                  }
-                }
+                      for (var pedido in widget.pedidos) {
+                        if (pedido.nome == widget.produto.nome) {
+                          pedido.adicionarPedido();
+                          pedidoExistente = true;
+                          break;
+                        }
+                      }
 
-                if (!pedidoExistente) {
-                  widget.pedidos.add(Pedido(
-                    nome: widget.produto.nome,
-                    descricao: widget.produto.descricao ?? '',
-                    valor: widget.produto.preco,
-                    quantidade: 1,
-                    imagem: widget.produto.foto,
-                  ));
-                }
+                      if (!pedidoExistente) {
+                        widget.pedidos.add(Pedido(
+                          nome: widget.produto.nome,
+                          descricao: widget.produto.descricao ?? '',
+                          valor: widget.produto.preco,
+                          quantidade: 1,
+                          imagem: widget.produto.foto,
+                        ));
+                      }
                 // Exibir um pop-up discreto dizendo "Nome do Produto adicionado ao carrinho"
-  ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(
-      content: Text(
-        '${widget.produto.nome} adicionado ao carrinho',
-        style: TextStyle(color: Colors.white), // Define a cor do texto
-      ),
-      duration: Duration(seconds: 1), // Define a duração do pop-up
-      behavior: SnackBarBehavior.fixed, // Define a animação como flutuante
-      backgroundColor: Colors.red, // Define a cor de fundo vermelho claro
-    ),
-  );
-};
-            },
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          '${widget.produto.nome} adicionado ao carrinho',
+                            style: TextStyle(color: Colors.white), // Define a cor do texto
+                          ),
+                          duration: Duration(seconds: 1), // Define a duração do pop-up
+                          behavior: SnackBarBehavior.fixed, // Define a animação como flutuante
+                          backgroundColor: Colors.red, // Define a cor de fundo vermelho claro
+                        ),
+                      );
+                    };
+                  },
             
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color.fromARGB(255, 244, 67, 54),
-              padding: const EdgeInsets.symmetric(
-                vertical: 10.0,
-                horizontal: 50,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 244, 67, 54),
+                    padding: const EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 50,
+                    ),
+                  ),
+                  child: const Text(
+                    'Adicionar',
+                    style: TextStyle(
+                      fontSize: 17.0,
+                      color: Colors.white, 
+                    ),
+                  ),
+                ),
               ),
-            ),
-            child: const Text(
-              'Adicionar',
-              style: TextStyle(
-                fontSize: 17.0,
-                color: Colors.white, 
-              ),
-            ),
+            ],
           ),
         ),
-      ],
-    ),
-  ),
-),
-
-
-
-
-      // bottomNavigationBar: BottomNavigationBar(
-      //   backgroundColor: const Color.fromARGB(255, 255, 0, 0),
-      //   selectedItemColor: Color.fromARGB(255, 0, 0, 0),
-      //   currentIndex: _currentIndex,
-      //   unselectedItemColor: Colors.white,
-      //   items: [
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.fastfood),
-      //       label: 'Cardápio',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.shopping_cart),
-      //       label: 'Carrinho',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.person),
-      //       label: 'Dados',
-      //     ),
-      //   ],
-      //   onTap: (index) {
-      //     switch (index) {
-      //       case 0:
-      //         //Navegue para alguma página
-      //         Navigator.push(
-      //                   context,
-      //                   MaterialPageRoute(builder: (context) => PaginaCardapio()),
-      //                 );
-      //         break;
-      //       case 1:
-      //         //Navegue para alguma página
-      //         //Navigator.pushReplacementNamed(context, '/pagina2');
-      //         break;
-      //       case 2:
-      //         //Navegue para alguma página
-      //         Navigator.push(
-      //                   context,
-      //                   MaterialPageRoute(builder: (context) => PaginaDados()),
-      //                 );
-      //         break;
-      //     }
-      //   },
-      // ),
+      ),
     );
   }
 }
